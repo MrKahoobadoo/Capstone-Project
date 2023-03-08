868b973acff67b16ed731cec2c28bddca5b12746
Mr. Kahoobadoo <60197309+MrKahoobadoo@users.noreply.github.com>
2023-03-08 10:22:44 -0500

    more bowling updates

--- Bowling/Assets/BowlingBall.cs
+++ Bowling/Assets/BowlingBall.cs
@@ -12,11 +12,47 @@ public class BowlingBall : MonoBehaviour
 
     void Start()
     {
-        Bowl();
+
     }
 
-    void Bowl()
+    public void Bowl()
     {
-        rig.AddForce(transform.forward * forwardForce);
+        rig.AddForce(transform.forward * forwardForce, ForceMode.Impulse);
+    }
+
+    public void MoveLeft()
+    {
+        if(transform.position.z > leftConstraint)
+        {
+            transform.position += new Vector3(0, 0, -moveIncrement);
+        }
+        
+    }
+
+    public void MoveRight()
+    {
+        if (transform.position.z < rightConstraint)
+        {
+            transform.position += new Vector3(0, 0, moveIncrement);
+        }
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Bowl();
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
     }
 }

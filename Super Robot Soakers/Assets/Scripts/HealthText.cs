8277c4dc507d51cfd472e3e5633072123cc7eb87
Mr. Kahoobadoo <60197309+MrKahoobadoo@users.noreply.github.com>
2023-03-27 12:18:50 -0400

    Worked on UI Stuff

--- Super Robot Soakers/Assets/Scripts/HealthText.cs
+++ Super Robot Soakers/Assets/Scripts/HealthText.cs
@@ -9,10 +9,19 @@ public class HealthText : MonoBehaviour
     public RealPlayerController realPlayerController;
     public TextMeshProUGUI healthText;
     public Slider slider;
+    public Image movingBar;
 
     private float curHP;
     private float maxHP;
     private float healthRatio;
+
+    private Color newColor = new Color(0f, 1f, 0f);
+
+    void Start()
+    {
+        curHP = realPlayerController.curHp;
+        maxHP = realPlayerController.maxHp;
+    }
 
     // Update is called once per frame
     void Update()
@@ -22,9 +31,21 @@ public class HealthText : MonoBehaviour
 
     void updateHealthText()
     {
-        healthText.text = realPlayerController.curHp + "/" + realPlayerController.maxHp;
-        healthRatio = (realPlayerController.curHp / (realPlayerController.maxHp)f);
+        curHP = realPlayerController.curHp;
+        maxHP = realPlayerController.maxHp;
+        healthText.text = curHP + "/" + maxHP;
+        healthRatio = (curHP / maxHP);
         slider.value = healthRatio * 280f;
-        //slider.value = realPlayerController.curHp;
+
+        if(curHP >= 50)
+        {
+            newColor = new Color(2f * (1f - healthRatio), 1f, 0f);
+        }
+        else
+        {
+            newColor = new Color(1f, healthRatio * 2f, 0f);
+        }
+        
+        movingBar.color = newColor;
     }
 }
